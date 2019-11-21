24c6fe5911a1566c28f769c8fcb7ae75f200361f
Cody Hillroy <codyhillroy@gmail.com>
2019-11-21 14:03:40 +0200

    remove redux devtools middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
@@ -10,10 +10,7 @@ import rootReducer from "./redusers/rootReducer";
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk),
 );
 
 ReactDOM.render(
